5cf83764a292eb321c0c358d22cbdc881d5fd73c
mrdong916 <mrdong916@163.com>
2020-02-20 14:44:39 +0800

    :bookmark: V0.0.3 fix bug

--- lib/flutter_android_downloader.dart
+++ lib/flutter_android_downloader.dart
@@ -9,7 +9,7 @@ class FlutterAndroidDownloader {
   static EventChannel _eventChannel =
   const EventChannel('com.bzqll.android_downloader/complete');
 
-  static StreamSubscription _streamSubscription;
+  static Stream _streamSubscription;
 
   static Future<int> download(String url, String directory,
       String fileName) async {
@@ -27,14 +27,9 @@ class FlutterAndroidDownloader {
 
   static void listen(Function callback) {
     // 当页面生成的时候就开始监听数据的变化
-    _streamSubscription = _eventChannel.receiveBroadcastStream().listen(
-          (data) {
-        callback(data);
-      },
-      cancelOnError: true,
-    );
-    _streamSubscription.onData((data) {
-      print(" data => $data");
+    _streamSubscription = _eventChannel.receiveBroadcastStream();
+    _streamSubscription.listen((data) {
+      callback(data);
     });
   }
 }
