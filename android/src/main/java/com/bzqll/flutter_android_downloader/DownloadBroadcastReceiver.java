9ce32dea9022f176a049825a2090c5f2249d968b
mrdong916 <mrdong916@163.com>
2020-02-24 14:19:10 +0800

    :bookmark: V0.0.3 fix bug

--- android/src/main/java/com/bzqll/flutter_android_downloader/DownloadBroadcastReceiver.java
+++ android/src/main/java/com/bzqll/flutter_android_downloader/DownloadBroadcastReceiver.java
@@ -5,10 +5,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.database.Cursor;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import io.flutter.plugin.common.EventChannel;
 
@@ -40,43 +36,9 @@ public class DownloadBroadcastReceiver extends BroadcastReceiver implements Even
             if(intent.getAction()!=null && intent.getAction().equals(ACTION_DOWNLOAD_COMPLETE)){
                 // 下载完成后发送给flutter下载完成的ID
                 long completeDownloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0L);
-                events.success(queryDownloadInfo(completeDownloadId));
+                events.success(completeDownloadId);
             }
         }
     }
-    public Map<String,Object> queryDownloadInfo(Long id) {
-        Map<String,Object> result = new HashMap<>();
-        // 创建一个查询对象
-        DownloadManager.Query query = new DownloadManager.Query().setFilterById(id);
-        // 根据查询对象获取一个游标对象
-        Cursor cursor = null;
-        try {
-            cursor = this.downloadManager.query(query);
-            if (null != cursor) {
-                if (cursor.moveToFirst()) {
-                    String uri = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_URI));
-                    int status = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS));
-                    String local_uri = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_LOCAL_URI));
-                    String total_size = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
-                    String media_type = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_MEDIA_TYPE));
-                    String title = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TITLE));
 
-                    result.put("id",id);
-                    result.put("uri",uri);
-                    result.put("status",status);
-                    result.put("local_uri",local_uri);
-                    result.put("media_type",media_type);
-                    result.put("total_size",total_size);
-                    result.put("title",title);
-                }
-            }
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-
-
-        return result;
-    }
 }
